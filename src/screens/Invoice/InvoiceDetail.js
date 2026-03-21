12d7c08f5bfd3f528077e14103e232e0707d677d
opermancode <jagtapomkar3052002@gmail.com>
2026-03-21 13:45:46 +0530

    Update InvoiceDetail.js

--- src/screens/Invoice/InvoiceDetail.js
+++ src/screens/Invoice/InvoiceDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, useCallback as useCallbackRef } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import {
   View, Text, StyleSheet, ScrollView, TouchableOpacity,
   Alert, Modal, TextInput, ActivityIndicator, Share,
