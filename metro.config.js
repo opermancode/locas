ca79e8850198f7051a5329594bee0ccd5e953f39
opermancode <jagtapomkar3052002@gmail.com>
2026-04-01 11:17:42 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Push 'wasm' into the asset extensions to resolve the expo-sqlite error
+// 1. Fix the .wasm error (Required for expo-sqlite)
 config.resolver.assetExts.push('wasm');
+
+// 2. Ensure Metro prioritizes .web.js files when bundling for web 
+// and ignores them when bundling for Android/iOS
+config.resolver.sourceExts.push('mjs'); 
 
 module.exports = config;
